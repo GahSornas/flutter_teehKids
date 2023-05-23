9d5d6548bb7268ae27d88e9f36343ce6dd5ad7f2
Gabriel <gabrielbsornas@gmail.com>
2023-05-22 21:19:46 -0300

    emergency page create

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,12 @@ import 'package:pi3_flutter_1/firebase_options.dart';
 // Widgets
 import 'package:pi3_flutter_1/preview_page.dart';
 import 'package:pi3_flutter_1/widgets/attachment.dart';
+import 'package:pi3_flutter_1/emergency_page.dart';
 
 // Storage
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path/path.dart' as path;
 import 'package:image_picker/image_picker.dart';
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -66,17 +66,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   //Function para enviar informações para o firestorage
   Future<void> launchInfoHelp(String name, String tel) async {
-      if(pic != File('assets/images/default_camera.png')){
-         await upload(pic.path);
+    if (pic != File('assets/images/default_camera.png')) {
+      await upload(pic.path);
       return infoHelp
-        .add({
-          'nome': name,
-          'telefone': tel,
-          'statusEmergencia': 'pendente',
-        })
-        .then((value) => debugPrint("Enviado com Sucesso!!"))
-        .catchError((error) => debugPrint("Erro ao adicionar: $error"));  
-      }
+          .add({
+            'nome': name,
+            'telefone': tel,
+            'statusEmergencia': 'pendente',
+          })
+          .then((value) => debugPrint("Enviado com Sucesso!!"))
+          .catchError((error) => debugPrint("Erro ao adicionar: $error"));
+    }
   }
 
   //foto
@@ -97,13 +97,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> upload(String path) async {
     File file = File(path);
-    try{
+    try {
       String ref = 'emergencias/2/img-${DateTime.now().toString()}.jpeg';
       await storage.ref(ref).putFile(file);
     } on FirebaseException catch (e) {
-        throw Exception(('Erro no upload: ${e.code}'));
+      throw Exception(('Erro no upload: ${e.code}'));
     }
-    
   }
 
   @override
@@ -116,81 +115,98 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Form(
         key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            //Label do nome
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20, top: 10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.account_circle),
-                  hintText: 'Nome',
-                  border: OutlineInputBorder(),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              //Label do nome
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20, top: 10),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.account_circle),
+                    hintText: 'Nome',
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Informe um nome';
+                    }
+                    return null;
+                  },
                 ),
-                controller: nameController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Informe um nome';
-                  }
-                  return null;
-                },
               ),
-            ),
 
-            //Label do telefone
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20, top: 20, bottom: 10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.call),
-                  hintText: 'Telefone',
-                  border: OutlineInputBorder(),
+              //Label do telefone
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20.0, right: 20, top: 20, bottom: 10),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.call),
+                    hintText: 'Telefone',
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: foneController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Informe um telefone';
+                    }
+                    return null;
+                  },
                 ),
-                controller: foneController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Informe um telefone';
-                  }
-                  return null;
-                },
               ),
-            ),
 
-            //botão foto
-            ElevatedButton.icon(
-              onPressed: () => Get.to(
-                () => CameraCamera(onFile: (pic) => showPreview(pic)),
+              //botão foto
+              ElevatedButton.icon(
+                onPressed: () => Get.to(
+                  () => CameraCamera(onFile: (pic) => showPreview(pic)),
+                ),
+                icon: const Icon(Icons.camera_alt),
+                label: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text('Tire uma foto'),
+                ),
+                style: ElevatedButton.styleFrom(
+                  elevation: 0.0,
+                  textStyle: const TextStyle(fontSize: 18),
+                ),
               ),
-              icon: const Icon(Icons.camera_alt),
-              label: const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text('Tire uma foto'),
-              ),
-              style: ElevatedButton.styleFrom(
-                elevation: 0.0,
-                textStyle: const TextStyle(fontSize: 18),
-              ),
-            ),
-            Attachment(pic: pic),
+              Attachment(pic: pic),
 
-            //btn gravar dados
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: () {  
-                      if (_formKey.currentState!.validate()) {
+              //btn gravar dados
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
                       launchInfoHelp(nameController.text, foneController.text);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const EmergenciesPage()),
+                      );
                       ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(
                             content: Text('Gravando dados no Firestore...')),
                       );
-                  }
-                },
-                child: const Text('Submit'),
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
               ),
-            ),
-          ],
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const EmergenciesPage()),
+                    );
+                  },
+                  child: const Text('proximo'))
+            ],
+          ),
         ),
       ),
     );
